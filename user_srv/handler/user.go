853d35fedf660a0fb649f9720b1353aee2dc476f
liuyb <liuyb@ickey.cn>
2022-05-31 23:52:32 +0800

    go srvs: user_srv.GetUserByMobile() & GetUserById() & CreateUser()

--- user_srv/handler/user.go
+++ user_srv/handler/user.go
@@ -2,20 +2,21 @@ package handler
 
 import (
 	"context"
+	"crypto/sha512"
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"gorm.io/gorm"
 
+	"github.com/anaskhan96/go-password-encoder"
 	"github.com/liuyongbing/hello-go-srvs/user_srv/global"
-
 	"github.com/liuyongbing/hello-go-srvs/user_srv/model"
 	"github.com/liuyongbing/hello-go-srvs/user_srv/proto"
 )
 
 //type UserServer interface {
-//	GetUserList(context.Context, *PageInfo) (*UserListResponse, error)
-//	GetUserByMobile(context.Context, *MobileRequest) (*UserInfoResponse, error)
-//	GetUserById(context.Context, *IdRequest) (*UserInfoResponse, error)
-//	CreateUser(context.Context, *CreateUserInfo) (*UserInfoResponse, error)
 //	UpdateUser(context.Context, *UpdateUserInfo) (*emptypb.Empty, error)
 //	CheckPassword(context.Context, *PasswordCheckInfo) (*CheckResponse, error)
 //	mustEmbedUnimplementedUserServer()
@@ -23,7 +24,10 @@ import (
 type UserServer struct {
 }
 
-// 返回数据结构绑定
+/*
+ModelToResponse
+返回数据结构绑定
+*/
 func ModelToResponse(user model.User) proto.UserInfoResponse {
 	userInfoRsp := proto.UserInfoResponse{
 		Id:       user.ID,
@@ -42,7 +46,10 @@ func ModelToResponse(user model.User) proto.UserInfoResponse {
 	return userInfoRsp
 }
 
-// 分页
+/*
+Paginate
+分页
+*/
 func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if page == 0 {
@@ -61,6 +68,10 @@ func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
+/*
+GetUserList
+获取用户列表
+*/
 func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*proto.UserListResponse, error) {
 	var users []model.User
 	result := global.DB.Find(&users)
@@ -82,4 +93,71 @@ func (s *UserServer) GetUserList(ctx context.Context, req *proto.PageInfo) (*pro
 	}
 
 	return rsp, nil
+}
+
+/*
+GetUserByMobile
+通过 Mobile 查询用户
+*/
+func (s *UserServer) GetUserByMobile(ctx context.Context, req *proto.MobileRequest) (*proto.UserInfoResponse, error) {
+	var user model.User
+	result := global.DB.Where(&model.User{Mobile: req.Mobile}).First(&user)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "用户不存在")
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	userInfoRsp := ModelToResponse(user)
+
+	return &userInfoRsp, nil
+}
+
+/*
+GetUserById
+通过 Id 查询用户
+*/
+func (s *UserServer) GetUserById(ctx context.Context, req *proto.IdRequest) (*proto.UserInfoResponse, error) {
+	var user model.User
+	result := global.DB.First(&user, req.Id)
+	if result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "用户不存在")
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	userInfoRsp := ModelToResponse(user)
+
+	return &userInfoRsp, nil
+}
+
+/*
+CreateUser
+创建用户
+*/
+func (s *UserServer) CreateUser(ctx context.Context, req *proto.CreateUserInfo) (*proto.UserInfoResponse, error) {
+	var user model.User
+	result := global.DB.Where(&model.User{Mobile: req.Mobile}).First(&user)
+	if result.RowsAffected == 1 {
+		return nil, status.Errorf(codes.AlreadyExists, "用户已存在")
+	}
+
+	user.Mobile = req.Mobile
+	user.Nickname = req.NickName
+
+	//密码加密
+	options := &password.Options{SaltLen: 16, Iterations: 100, KeyLen: 32, HashFunction: sha512.New}
+	salt, encodedPwd := password.Encode(req.PassWord, options)
+	user.Password = fmt.Sprintf("$pbkdf2-sha512$%s$%s", salt, encodedPwd)
+
+	result = global.DB.Create(&user)
+	if result.Error != nil {
+		return nil, status.Errorf(codes.Internal, result.Error.Error())
+	}
+
+	userInfoRsp := ModelToResponse(user)
+
+	return &userInfoRsp, nil
 }
