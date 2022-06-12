fc498c588962d349993964ec26332bea752da6be
liuyb <liuyb@ickey.cn>
2022-06-12 09:36:00 +0800

    go srv: consul register

--- user_srv/main.go
+++ user_srv/main.go
@@ -9,54 +9,12 @@ import (
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
-	"github.com/hashicorp/consul/api"
-
 	"github.com/liuyongbing/hello-go-srvs/user_srv/global"
 	"github.com/liuyongbing/hello-go-srvs/user_srv/handler"
 	"github.com/liuyongbing/hello-go-srvs/user_srv/initialize"
 	"github.com/liuyongbing/hello-go-srvs/user_srv/proto"
+	"github.com/liuyongbing/hello-go-srvs/user_srv/utils"
 )
-
-/*
-Register
-服务注册
-*/
-func Register(addr string, port int, name string, tags []string, id string) {
-	consulInfo := global.ServerConfig.ConsulInfo
-
-	cfg := api.DefaultConfig()
-	// cfg.Address = "127.0.0.1:8500"
-	cfg.Address = fmt.Sprintf("%s:%d", consulInfo.Host, consulInfo.Port)
-
-	client, err := api.NewClient(cfg)
-	if err != nil {
-		panic(err)
-	}
-
-	// 服务健康检查对象
-	check := api.AgentServiceCheck{
-		GRPC:                           fmt.Sprintf("%s:%d", addr, port),
-		Timeout:                        "5s",
-		Interval:                       "5s",
-		DeregisterCriticalServiceAfter: "10s",
-	}
-
-	// 服务注册对象
-	registration := api.AgentServiceRegistration{
-		Name:    name,
-		ID:      id,
-		Tags:    tags,
-		Port:    port,
-		Address: addr,
-		Check:   &check,
-	}
-
-	// 注册服务
-	err = client.Agent().ServiceRegister(&registration)
-	if err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	// 初始化
@@ -91,7 +49,7 @@ func main() {
 		"gosrv-register",
 		"consul",
 	}
-	Register(addr, port, name, tags, id)
+	utils.Register(addr, port, name, tags, id)
 
 	err = server.Serve(lis)
 	if err != nil {
